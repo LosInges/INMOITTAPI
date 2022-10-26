6cab713ddd87854f44582fd822987e329db40ed2
Ximena <xivillegassa@ittepic.edu.mx>
2022-10-26 09:43:54 -0600

    getInmobiliaria

--- InmobiliariasRoutes.js
+++ InmobiliariasRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const postInmobiliaria = require("./controllers/postInmobiliaria");
+const getInmobiliaria = require ("./controllers/getInmobiliaria");
 const router = express.Router();
 /// get
+    router.get('/inmobiliaria/:correo',getInmobiliaria.getInmobiliaria)
 /// post 
     router.post('/inmobiliaria',postInmobiliaria.postInmobiliaria)
 /// delete
