10f5b6089c123225268e09d1f45ce50aee5c014d
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-10-25 22:48:12 -0600

    get Clientes

--- controllers/getCliente.js
+++ controllers/getCliente.js
@@ -1,1 +1,18 @@
 const conectar = require("./conexion");
+
+const getCliente = (req, res) => {
+  conectar.execute(
+    "SELECT * FROM clientes where correo=?",
+    [req.params.correo],
+    { prepare: true },
+    (err, results) => {
+      if (err) {
+        res.header("Access-Control-Allow-Origin", "*").json({ err });
+        return;
+      }
+      res.header("Access-Control-Allow-Origin", "*").send(results.rows[0]);
+    }
+  );
+};
+
+module.exports={getCliente};
