15d1579c12d6c6017a2ecc251446bd1ab0cb57f5
Ximena <xivillegassa@ittepic.edu.mx>
2022-10-27 09:59:15 -0600

    API delete Inmobiliaria

--- InmobiliariasRoutes.js
+++ InmobiliariasRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const postInmobiliaria = require("./controllers/postInmobiliaria");
 const getInmobiliaria = require ("./controllers/getInmobiliaria");
+const deleteInmobiliaria = require("./controllers/deleteInmobiliaria");
 const router = express.Router();
 /// get
     router.get('/inmobiliaria/:correo',getInmobiliaria.getInmobiliaria)
 /// post 
     router.post('/inmobiliaria',postInmobiliaria.postInmobiliaria)
 /// delete
+    router.delete('/inmobiliaria',deleteInmobiliaria.deleteInmobiliaria)
 module.exports=router;
