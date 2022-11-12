f33ada97e1b4b2c7da0e34651a39432944054610
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-11-12 04:10:43 -0700

    MAS AJUSTES IMAGENES

--- controllers/imagenes.js
+++ controllers/imagenes.js
@@ -1,26 +1,21 @@
-const express = require('express');
 const sharp = require('sharp');
-const conexion = require('./conexion');
 
 const img = (req, res) => {
   sharp(req.file.path)
     .clone()
     .toFile(`public/img/${req.file.filename}`, (err, info) => {
       if (err) {
-        res.header("Access-Control-Allow-Origin", "*").send({ err, ok: false });
+        res.header('Access-Control-Allow-Origin', '*').send({ err, ok: false });
       } else {
-        conexion.execute(
-          'INSERT INTO imagenes_inmueble(titulo, ruta) VALUES(?,?)',
-          [req.body.titulo, `public/img/${req.file.filename}`],
-          { prepare: true },
-          (err, results) => {
-            if (err) {
-              res.header("Access-Control-Allow-Origin", "*").send({ err, ok: false });
-            } else {
-              res.header("Access-Control-Allow-Origin", "*").send({ ok: true, path: `img/${req.file.filename}` });
-            }
-          }
-        );
+        if (err) {
+          res
+            .header('Access-Control-Allow-Origin', '*')
+            .send({ err, ok: false });
+        } else {
+          res
+            .header('Access-Control-Allow-Origin', '*')
+            .send({ ok: true, path: `img/${req.file.filename}` });
+        }
       }
     });
 };
@@ -28,27 +23,20 @@ const img = (req, res) => {
 const imgMiniatura = (req, res) => {
   let cargas = [
     sharp(req.file.path)
-      .resize(250, 250)
+      .resize(64, 64)
       .toFile(`public/img/miniaturas/${req.file.filename}`),
     sharp(req.file.path).clone().toFile(`public/img/${req.file.filename}`),
   ];
   Promise.all(cargas)
     .then(() => {
-      conexion.execute(
-        'INSERT INTO imagenes_inmueble(titulo, ruta) VALUES(?,?)',
-        [req.body.titulo, `public/img/${req.file.filename}`],
-        { prepare: true },
-        (err, results) => {
-          if (err) {
-            res.header("Access-Control-Allow-Origin", "*").send({ err, ok: false });
-          } else {
-            res.header("Access-Control-Allow-Origin", "*").send({ ok: true, path: `img/${req.file.filename}` });
-          }
-        }
-      );
+      res.header('Access-Control-Allow-Origin', '*').send({
+        ok: true,
+        path: `img/${req.file.filename}`,
+        miniatura: `img/miniaturas/${req.file.filename}`,
+      });
     })
     .catch((err) => {
-      res.header("Access-Control-Allow-Origin", "*").send({ err, ok: false });
+      res.header('Access-Control-Allow-Origin', '*').send({ err, ok: false });
     });
 };
 
@@ -57,9 +45,11 @@ const miniatura = (req, res) => {
     .resize(64, 64)
     .toFile(`public/img/miniaturas/${req.file.filename}`, (err, info) => {
       if (err) {
-        res.header("Access-Control-Allow-Origin", "*").send({ err, ok: false });
+        res.header('Access-Control-Allow-Origin', '*').send({ err, ok: false });
       } else {
-        res.header("Access-Control-Allow-Origin", "*").send({ ok: true, path: `img/miniaturas/${req.file.filename}` });
+        res
+          .header('Access-Control-Allow-Origin', '*')
+          .send({ ok: true, path: `img/miniaturas/${req.file.filename}` });
       }
     });
 };
